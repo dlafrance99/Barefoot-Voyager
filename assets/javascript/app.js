ec116279f62371faff0b889274525729ec451e15
Jake Garlick <jacobgarlick@msn.com>
2019-09-23 11:16:35 -0600

    additional changes to JSON functionality based on user destination and user interest

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-// Grabs information from user input on forms
+// Grabs information from user input on forms after submit button is clicked 
 
 $(".button-primary").on("click", function(event) {
     event.preventDefault();
@@ -7,7 +7,34 @@ $(".button-primary").on("click", function(event) {
     console.log(userLocation);
     var userDates = $("#date-input").val().trim();
     console.log(userDates);
-    var userInterests = $("#interests-input").val().trim();
+    var userInterests = $("#interest-input").val().trim();
     console.log(userInterests);
 
 });
+
+// Tie into YouTube API and display videos based on userLocation and userInterests
+
+function youTubeAPICall () {
+    var locationYouTube = "Denver";
+    var userInterestYouTube = "Football"
+    var searchYouTube = locationYouTube + " " + userInterestYouTube;
+
+    var queryURLYouTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchYouTube + "&key=AIzaSyB8DRZm65uhLchA5CJ7k_6od-xh013ofd8";
+
+    $.ajax({
+        url: queryURLYouTube,
+        method: "GET"
+      }).then(function (response) {
+        console.log(response)
+
+})};
+
+youTubeAPICall();
+
+// curl \
+//   'https://www.googleapis.com/youtube/v3/search?part=snippet&location=denver&locationRadius=10m&q=blue&videoEmbeddable=true&key=[YOUR_API_KEY]' \
+//   --header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
+//   --header 'Accept: application/json' \
+//   --compressed
+
+
