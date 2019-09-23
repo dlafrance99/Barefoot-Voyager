a154d728059646f981222711e70f0b9d487c976c
Rebecca Barrett <barrett4467@gmail.com>
2019-09-23 12:13:54 -0600

    Adding date picker validation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,13 @@
+//   $('input[name="dates"]').daterangepicker({
+//     "showDropdowns": true,
+//     "minYear": 1900,
+//     "linkedCalendars": false,
+//     "parentEl": "#date-input",
+//     "startDate": "09/17/2019",
+//     "endDate": "09/23/2019"
+// }, function(start, end, label) {
+//   console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+// });
 
+
+$('input[name="dates"]').daterangepicker();
