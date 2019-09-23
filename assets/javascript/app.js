c4f4eca29ca3354c6556039f129550eefa6bd03b
Jake Garlick <jacobgarlick@msn.com>
2019-09-23 13:03:36 -0600

    fixed embed functionality for youTube searches based on destination and interests

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,31 @@
+// Global Variables
+
+var userLocation;
+var userDates;
+var userInterests;
+
+
 // Grabs information from user input on forms after submit button is clicked 
 
 $(".button-primary").on("click", function(event) {
     event.preventDefault();
 
-    var userLocation = $("#location-input").val().trim();
+    userLocation = $("#location-input").val().trim();
     console.log(userLocation);
-    var userDates = $("#date-input").val().trim();
+    userDates = $("#date-input").val().trim();
     console.log(userDates);
-    var userInterests = $("#interest-input").val().trim();
+    userInterests = $("#interest-input").val().trim();
     console.log(userInterests);
 
+    $(".youtube-insert").empty();
+    youTubeAPICall();
 });
 
 // Tie into YouTube API and display videos based on userLocation and userInterests
 
 function youTubeAPICall () {
-    var locationYouTube = "Denver";
-    var userInterestYouTube = "Music"
+    var locationYouTube = userLocation;
+    var userInterestYouTube = userInterests;
     var searchYouTube = locationYouTube + " " + userInterestYouTube;
 
     var queryURLYouTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchYouTube + "&type=video&key=AIzaSyB8DRZm65uhLchA5CJ7k_6od-xh013ofd8";
@@ -30,37 +39,20 @@ function youTubeAPICall () {
           
           for (var i = 0; i < 5; i++){
             var youTubeVideoId = response.items[i].id.videoId
-            var youTubePageAdd = "https://www.youtube.com/embed?v=" + youTubeVideoId;
-            var newDivide = $("<div>")
+            var youTubePageAdd = "https://www.youtube.com/embed/" + youTubeVideoId;
+            var newDivideYouTube = $("<p>");
 
             var youTubePlace = $("<iframe>")
-            youTubePlace.attr("width", "560");
-            youTubePlace.attr("height", "315");
+            youTubePlace.attr("width", "360");
+            youTubePlace.attr("height", "215");
             youTubePlace.attr("src", youTubePageAdd);
             youTubePlace.attr("frameborder", "0");
             youTubePlace.attr("allow", "accelerometer; encrypted-media; gyroscope; picture-in-picture");
 
-            
-            newDivide.append(youTubePlace);
-            $(".youtube-insert").append(newDivide);
+            newDivideYouTube.append(youTubePlace);
+            $(".youtube-insert").append(newDivideYouTube);
             
         };
-        // var youTubeEmbed = $(".youtube-embed").attr({
-        //     width: "560",
-        //     heigh: "315",
-        //     src: youTubePageAdd,
-        //     frameborder: "0",
-        //     allow: "accelerometer; encrypted-media; gyroscope; picture-in-picture",
-        // })
-
-
-        // newDivide.append(youTubeEmbed);
-        // $(".youtube-insert").append(newDivide);
-
-        
-        // <iframe width="560" height="315" src="https://www.youtube.com/embed/oxNgpumIWdQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 })};
-
-youTubeAPICall();
 
