d1b9ff7facb69f17e9820b4750f278c7abb3ab96
Devon LaFrance <lafrance.devon@gmail.com>
2019-09-23 11:59:36 -0600

    news api functioning, will still need some fine tuning and css styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // news api 
-$("#submit").on("click", function(event){
+$("#submit").on("click", function (event) {
     event.preventDefault();
 
 
@@ -8,22 +8,39 @@ $("#submit").on("click", function(event){
     var newsAPIkey = "3fe53b99010243faa5ad7667a9f9d73f";
     var newsURL = "https://newsapi.org/v2/everything?q=" + location + "&apiKey=3fe53b99010243faa5ad7667a9f9d73f&pageSize=" + articleLimit;
 
-    console.log(newsURL);
 
-   $.ajax({
-       url: newsURL,
-       method: "GET",
-   }).then(function(response){
+    $.ajax({
+        url: newsURL,
+        method: "GET",
+    }).then(function (response) {
         var articles = response.articles;
-        for (var i=0; i<articles.length; i++){
+        console.log(articles);
+        var newsDiv = $("<div>");
+        newsDiv.addClass("newsDiv")
+        for (var i = 0; i < articles.length; i++) {
             var headline = articles[i].title;
             var byline = articles[i].author;
             var source = articles[i].source.name;
             var articlepictureURL = articles[i].urlToImage;
-
+            var articleNumber = i+1;
+            var articleUrl = articles[i].url;
             
-        }
-   });
+
+
+
+            var articleImage = $("<img>");
+            articleImage.attr("src", articlepictureURL);
+            articleImage.addClass("newsImg")
+
+            newsDiv.append("<h5 id='headline'>" + articleNumber + ") <a href='"+ articleUrl + "'>" + headline + "</a></h5>");
+            newsDiv.append("<h6 id='byline'>By: " + byline + "</h6>");
+            newsDiv.append("<h6 id='source'>Source: " + source + "</h6>");
+
+            newsDiv.append(articleImage);
+
+        };
+        $(".content").html(newsDiv);
+    });
 
 
 
