c0acc5a4721d8cab7c3afe4d22c5ad1e78256912
Jake Garlick <jacobgarlick@msn.com>
2019-09-23 12:45:51 -0600

    began adding buildout for embed ability for youTube videos

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,25 +16,51 @@ $(".button-primary").on("click", function(event) {
 
 function youTubeAPICall () {
     var locationYouTube = "Denver";
-    var userInterestYouTube = "Football"
+    var userInterestYouTube = "Music"
     var searchYouTube = locationYouTube + " " + userInterestYouTube;
 
-    var queryURLYouTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchYouTube + "&key=AIzaSyB8DRZm65uhLchA5CJ7k_6od-xh013ofd8";
+    var queryURLYouTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchYouTube + "&type=video&key=AIzaSyB8DRZm65uhLchA5CJ7k_6od-xh013ofd8";
 
     $.ajax({
         url: queryURLYouTube,
         method: "GET"
       }).then(function (response) {
-        console.log(response)
+          console.log(response.items[0])
+          console.log(response.items[0].id.videoId)
+          
+          for (var i = 0; i < 5; i++){
+            var youTubeVideoId = response.items[i].id.videoId
+            var youTubePageAdd = "https://www.youtube.com/embed?v=" + youTubeVideoId;
+            var newDivide = $("<div>")
+
+            var youTubePlace = $("<iframe>")
+            youTubePlace.attr("width", "560");
+            youTubePlace.attr("height", "315");
+            youTubePlace.attr("src", youTubePageAdd);
+            youTubePlace.attr("frameborder", "0");
+            youTubePlace.attr("allow", "accelerometer; encrypted-media; gyroscope; picture-in-picture");
+
+            
+            newDivide.append(youTubePlace);
+            $(".youtube-insert").append(newDivide);
+            
+        };
+        // var youTubeEmbed = $(".youtube-embed").attr({
+        //     width: "560",
+        //     heigh: "315",
+        //     src: youTubePageAdd,
+        //     frameborder: "0",
+        //     allow: "accelerometer; encrypted-media; gyroscope; picture-in-picture",
+        // })
+
+
+        // newDivide.append(youTubeEmbed);
+        // $(".youtube-insert").append(newDivide);
+
+        
+        // <iframe width="560" height="315" src="https://www.youtube.com/embed/oxNgpumIWdQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 })};
 
 youTubeAPICall();
-
-// curl \
-//   'https://www.googleapis.com/youtube/v3/search?part=snippet&location=denver&locationRadius=10m&q=blue&videoEmbeddable=true&key=[YOUR_API_KEY]' \
-//   --header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
-//   --header 'Accept: application/json' \
-//   --compressed
-
 
