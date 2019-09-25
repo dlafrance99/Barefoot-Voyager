8082e298d2fb7412c9444e1e9eea782c4b8b9762
Devon LaFrance <lafrance.devon@gmail.com>
2019-09-25 09:54:37 -0600

    updated div format so things will come into the right order when called from the api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,26 +22,27 @@ $("#submit").on("click", function (event) {
 
     if ($("#location-input").hasClass("valid") && $("#date-input").hasClass("valid") && $("#interest-input").hasClass("valid")) {
 
+        divCreator();
+        newsAPICall();
+        ticketMasterAPICall();
         youTubeAPICall();
         openWeatherAPICall();
-        ticketMasterAPICall();
-        newsAPICall();
         breweryAPICall();
-
+        
     } else {
-
+        
         $("<p> Invalid selection, Please try again!</p>").modal();
-
+        
     }
 });
 
 $("#search-again").on("click", function (event) {
     event.preventDefault();
-
+    
     $("#location-input").val("");
     $("#date-input").val("");
     $("#interest-input").val("");
-
+    
     $("#weather-div").empty();
     $(".forecast").empty();
     $(".newsDiv").empty();
@@ -51,92 +52,17 @@ $("#search-again").on("click", function (event) {
     $(".content").empty();
 });
 
-// Tie into YouTube API and display videos based on userLocation and userInterests
-
-function youTubeAPICall() {
-    var locationYouTube = userLocation;
-    var userInterestYouTube = userInterests;
-    var searchYouTube = locationYouTube + " " + userInterestYouTube;
-
-    var queryURLYouTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchYouTube + "&type=video&key=AIzaSyB8DRZm65uhLchA5CJ7k_6od-xh013ofd8";
-
-    $.ajax({
-        url: queryURLYouTube,
-        method: "GET"
-    }).then(function (response) {
-
-        var newDivideYouTube = $("<div id='youtube-div'>");
-        var videoDiv = $("<div class='video'>");
-        for (var i = 0; i < 3; i++) {
-            var youTubeVideoId = response.items[i].id.videoId
-            var youTubePageAdd = "https://www.youtube.com/embed/" + youTubeVideoId;
-
-            var youTubePlace = $("<iframe>")
-            youTubePlace.attr("width", "auto");
-            youTubePlace.attr("height", "auto");
-            youTubePlace.attr("src", youTubePageAdd);
-            youTubePlace.attr("frameborder", "0");
-            youTubePlace.attr("allow", "accelerometer; encrypted-media; gyroscope; picture-in-picture");
-
-            videoDiv.append(youTubePlace);
-        };
-        newDivideYouTube.append(videoDiv);
-        newDivideYouTube.addClass("youtube-div");
-        $(".content").append(newDivideYouTube);
-        $(".youtube-div").prepend("<h4 id='youtube-header'>YouTube Videos</h4>")
-
-    })
-};
-
-// Tie into OpenWeather API and display current weather based on userLocation
-
-function openWeatherAPICall() {
-    var locationWeather = userLocation;
-    var apiKeyWeather = "cbe15fe8bd11f0165e29631925aca3d4";
-
-    var queryURLOpenWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + locationWeather + "&appid=" + apiKeyWeather + "&units=imperial";
-    var queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + locationWeather + "&appid=" + apiKeyWeather + "&units=imperial";
-
-    // This AJAX calls the current weather for the destination city based on userLocation
-
-    $.ajax({
-        url: queryURLOpenWeather,
-        method: "GET"
-    }).then(function (response) {
-
-        var weatherOverlay = $("<div class='weather-div'>")
-
-        var infoWeatherOverlay = `<p>Today's Weather Information for ${response.name}</p><p>Temperature: ${response.main.temp} F</p><p>High Temperature: ${response.main.temp_max} F</p><p>Low Temperature: ${response.main.temp_min} F</p><p>Wind Speed ${response.wind.speed} mph</p><p>Current Conditions: ${response.weather[0].description}</p><hr><p>5 Day Forecast for ${response.name}<p><hr>`;
-
-        weatherOverlay.append(infoWeatherOverlay);
-        $(".content").append(weatherOverlay);
-    });
-
-    // This AJAX calls the forecasted weather for the destination city based on userLocation
-
-    $.ajax({
-        url: queryURLForecast,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response)
-
-        for (var i = 7; i < 40; i += 8) {
-
-            var weatherForecast = (moment(response.list[i].dt_txt, "YYYY-MM-DD h:mm:ss").format("dddd, MMMM Do, h:mma"));
-            console.log(weatherForecast)
-
-            var forecastOverlay = $("<div class='forecast-div'>");
-
-            var forecastWeatherOverlay = `<p>Date: ${weatherForecast}</p><p>Temperature: ${response.list[i].main.temp} F</p><p>Current Conditions: ${response.list[i].weather[0].description}</p><p>Wind Speed: ${response.list[i].wind.speed} mph</p><hr>`
-
-            forecastOverlay.append(forecastWeatherOverlay);
-            $(".content").append(forecastOverlay);
-
-        }
-
-    });
-};
-
+function divCreator() {
+    var contentbox = $("<div class='content-box'>");
+    contentbox.append("<div id='newsBox'>");
+    contentbox.append("<div id='ticketmasterBox'>");
+    contentbox.append("<div id='youTubeBox'>");
+    contentbox.append("<div id='weatherBox'>");
+    contentbox.append("<div id='forecastBox'>");
+    contentbox.append("<div id='breweryBox'>");
+    
+    $(".content").append(contentbox);
+}
 // news api 
 function newsAPICall() {
 
@@ -188,7 +114,7 @@ function newsAPICall() {
 
             };
             newsDiv.prepend("<h4 id='news-header'>Top News</h4>")
-            $(".content").append(newsDiv);
+            $("#newsBox").append(newsDiv);
         });
 
     } else {
@@ -197,6 +123,121 @@ function newsAPICall() {
 
     }
 };
+
+// added the TM
+
+function ticketMasterAPICall() {
+
+    var location = $("#location-input").val().trim();
+    var Limit= 5;
+    var ticketMasterAPIkey = "TFjXDEI1LogVpEJmc428NgcftKE2zdS6f";
+    var ticketURL = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + location + "&apikey=TFjXDEI1LogVpEJmc428NgcftKE2zdS6";
+
+    $.ajax({
+        url: ticketURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            var results = response._embedded.events;
+            console.log(results);
+            var ticketsDiv = $("<div class='ticketmaster-div'>");
+            
+            for (var i = 0; i < 5; i++) {
+                var pname= $("<p>").text(results[i].name);
+                $(ticketsDiv).append(pname)
+                var pdate= $("<p>").text(results[i].dates.start.localDate);
+                $(ticketsDiv).append(pdate);
+            }
+            $("#ticketmasterBox").append(ticketsDiv)
+        })
+};
+
+// Tie into YouTube API and display videos based on userLocation and userInterests
+
+function youTubeAPICall() {
+    var locationYouTube = userLocation;
+    var userInterestYouTube = userInterests;
+    var searchYouTube = locationYouTube + " " + userInterestYouTube;
+
+    var queryURLYouTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchYouTube + "&type=video&key=AIzaSyB8DRZm65uhLchA5CJ7k_6od-xh013ofd8";
+
+    $.ajax({
+        url: queryURLYouTube,
+        method: "GET"
+    }).then(function (response) {
+
+        var newDivideYouTube = $("<div id='youtube-div'>");
+        var videoDiv = $("<div class='video'>");
+        for (var i = 0; i < 3; i++) {
+            var youTubeVideoId = response.items[i].id.videoId
+            var youTubePageAdd = "https://www.youtube.com/embed/" + youTubeVideoId;
+
+            var youTubePlace = $("<iframe>")
+            youTubePlace.attr("width", "auto");
+            youTubePlace.attr("height", "auto");
+            youTubePlace.attr("src", youTubePageAdd);
+            youTubePlace.attr("frameborder", "0");
+            youTubePlace.attr("allow", "accelerometer; encrypted-media; gyroscope; picture-in-picture");
+
+            videoDiv.append(youTubePlace);
+        };
+        newDivideYouTube.append(videoDiv);
+        newDivideYouTube.addClass("youtube-div");
+        $(".youtube-div").prepend("<h4 id='youtube-header'>YouTube Videos</h4>")
+        $("#youTubeBox").append(newDivideYouTube);
+
+    })
+};
+
+// Tie into OpenWeather API and display current weather based on userLocation
+
+function openWeatherAPICall() {
+    var locationWeather = userLocation;
+    var apiKeyWeather = "cbe15fe8bd11f0165e29631925aca3d4";
+
+    var queryURLOpenWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + locationWeather + "&appid=" + apiKeyWeather + "&units=imperial";
+    var queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + locationWeather + "&appid=" + apiKeyWeather + "&units=imperial";
+
+    // This AJAX calls the current weather for the destination city based on userLocation
+
+    $.ajax({
+        url: queryURLOpenWeather,
+        method: "GET"
+    }).then(function (response) {
+
+        var weatherOverlay = $("<div class='weather-div'>")
+
+        var infoWeatherOverlay = `<p>Today's Weather Information for ${response.name}</p><p>Temperature: ${response.main.temp} F</p><p>High Temperature: ${response.main.temp_max} F</p><p>Low Temperature: ${response.main.temp_min} F</p><p>Wind Speed ${response.wind.speed} mph</p><p>Current Conditions: ${response.weather[0].description}</p><hr><p>5 Day Forecast for ${response.name}<p><hr>`;
+
+        weatherOverlay.append(infoWeatherOverlay);
+        $("#weatherBox").append(weatherOverlay);
+    });
+
+    // This AJAX calls the forecasted weather for the destination city based on userLocation
+
+    $.ajax({
+        url: queryURLForecast,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+
+        for (var i = 7; i < 40; i += 8) {
+
+            var weatherForecast = (moment(response.list[i].dt_txt, "YYYY-MM-DD h:mm:ss").format("dddd, MMMM Do, h:mma"));
+            console.log(weatherForecast)
+
+            var forecastOverlay = $("<div class='forecast-div'>");
+
+            var forecastWeatherOverlay = `<p>Date: ${weatherForecast}</p><p>Temperature: ${response.list[i].main.temp} F</p><p>Current Conditions: ${response.list[i].weather[0].description}</p><p>Wind Speed: ${response.list[i].wind.speed} mph</p><hr>`
+
+            forecastOverlay.append(forecastWeatherOverlay);
+            $("#forecastBox").append(forecastOverlay);
+
+        }
+
+    });
+};
+
 
 // Brewery API Call
 function breweryAPICall() {
@@ -220,7 +261,8 @@ function breweryAPICall() {
                     breweryDiv.append("<p>" + street + "</p>");
 
                 }
-                $(".content").after(breweryDiv);
+                breweryDiv.addClass("brewery-div");
+                $("#breweryBox").append(breweryDiv);
             })
         }
         breweryApi();
@@ -286,65 +328,12 @@ console.log($("#interest-input").hasClass("valid"));
 // $("#location-input").hasClass("valid") && $("#date-input").hasClass("valid") && $("#interest-input").hasClass("valid")
 // $('input[name="dates"]').daterangepicker();
 
-// added the TM
 
-function ticketMasterAPICall() {
-
-    var location = $("#location-input").val().trim();
-    var Limit= 5;
-    var ticketMasterAPIkey = "TFjXDEI1LogVpEJmc428NgcftKE2zdS6f";
-    var ticketURL = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + location + "&apikey=TFjXDEI1LogVpEJmc428NgcftKE2zdS6";
-
-    $.ajax({
-        url: ticketURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            var results = response._embedded.events;
-            console.log(results);
-            var ticketsDiv = $("<div class='ticketmaster-div'>");
-            
-            for (var i = 0; i < 5; i++) {
-                var pname= $("<p>").text(results[i].name);
-                $(ticketsDiv).append(pname)
-                var pdate= $("<p>").text(results[i].dates.start.localDate);
-                $(ticketsDiv).append(pdate);
-            }
-            $(".content").append(ticketsDiv)
-        })
-};
 
 console.log($("#location-input").hasClass("valid"));
 console.log($("#date-input").hasClass("valid"));
 console.log($("#interest-input").hasClass("valid"));
 
-// $("#location-input").hasClass("valid") && $("#date-input").hasClass("valid") && $("#interest-input").hasClass("valid")
-// $('input[name="dates"]').daterangepicker();
 
-// Ticket Master API Call
-// function ticketMasterAPICall() {
-
-//     var location = $("#location-input").val().trim();
-//     var ticketMasterAPIkey = "TFjXDEI1LogVpEJmc428NgcftKE2zdS6f";
-//     var ticketURL = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + location + "&apikey=TFjXDEI1LogVpEJmc428NgcftKE2zdS6";
-
-//     $.ajax({
-//         url: ticketURL,
-//         method: "GET"
-//     })
-//         .then(function (response) {
-//             var results = response._embedded.events;
-//             console.log(results);
-//             var ticketsDiv = $("<div>");
-
-//             for (var i = 0; i < results.length; i++) {
-//                 var pname = $("<p>").text(results[i].name);
-//                 $(ticketsDiv).append(pname)
-//                 var pdate = $("<p>").text(results[i].dates.start.localDate);
-//                 $(ticketsDiv).append(pdate);
-//             }
-//             $(".ticket-information").html(ticketsDiv)
-//         })
-// };
 
 validation();
